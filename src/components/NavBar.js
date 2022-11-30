f62a0d32226b420d381ca91dbaf569184b1f8a68
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-11-30 18:50:47 +0000

    add routing, wire up links and add css file app and Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Navbar, Container, Nav} from "react-bootstrap";
-
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return(
-    <Navbar bg="primary" variant="dark" expand="md" fixed='top'>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar className={styles.NavBar} bg="primary" variant="dark" expand="md" fixed='top'>
+      <NavLink to="/"><Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand></NavLink>   
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                <Nav className="ml-auto text-left">
+                  <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">Home</NavLink>
+                  <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">Sign in</NavLink>
+                  <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">Sign up</NavLink>  
                 </Nav>
             </Navbar.Collapse>
     </Navbar>
---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Form} from 'react-bootstrap'
+
+const SignIn = () => {
+  return (
+    <Form>
+  <Form.Group controlId="formGroupEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+  <Form.Group controlId="formGroupPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+</Form>
+  )
+}
+
+export default SignIn
